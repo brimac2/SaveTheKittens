5579247ec99e5dc76bbb2e458cfe16d03f649f23
Brigita Macaite <brimacaite@gmail.com>
2022-01-03 15:05:08 +0100

    Initial Commit

---
+++ SaveTheKittens/SaveTheKittensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKittensApp.swift
+//  SaveTheKittens
+//
+//  Created by Brigita Macaitė on 2022-01-03.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKittensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
